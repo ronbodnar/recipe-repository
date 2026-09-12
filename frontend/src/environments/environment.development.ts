da4099ee3875dc7049dded0fe5712e61e4e43e5b
Ron Bodnar <ron.bodnar@outlook.com>
2026-09-12 02:15:12 -0700

    fix: use correct production API url

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -2,9 +2,4 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api/v1',
   cdnUrl: 'https://d3w0olhduc0wyv.cloudfront.net',
-  keycloak: {
-    realm: 'recipe-repository',
-    url: 'http://localhost:9000',
-    clientId: 'recipe-repository',
-  },
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,10 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://recipes.ronbodnar.com/gateway',
+  apiUrl: 'https://recipes.ronbodnar.com/api/v1',
   cdnUrl: 'https://d3w0olhduc0wyv.cloudfront.net',
-  keycloak: {
-    realm: 'recipe-repository',
-    url: 'https://recipes.ronbodnar.com/auth',
-    clientId: 'recipe-repository-frontend',
-  },
 };
